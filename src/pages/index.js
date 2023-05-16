74e836de498bcc5c3409b4a633988ba7e7ab9d38
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-05-16 00:00:15 -0500

    ajustes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      Hola
+      <a className=' underline' href='https://iter360.com/'>Visitanos en ITER360</a>
     </main>
   )
 }
