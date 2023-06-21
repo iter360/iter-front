6f9b6c148d6ee0297dc7eabaffee25c35bf83e4d
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-06-20 21:39:37 -0500

    dasdasdas

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://iter360.com/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
