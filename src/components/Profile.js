19708d1bb6a853402b481f76f54acba934dc0b6d
cristhian vargas <cristhian.a.vargas@gmail.com>
2023-06-20 21:21:53 -0500

    ajustes de alto de imagen

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,7 +15,7 @@ const Profile = ({ profileData }) => {
     <>
       <div className="-space-x-1 mt-[-35%] relative z-10 shadow-lg rounded-3xl bg-white overflow-hidden">
         <div className="flex justify-center z-10">
-          <Link href={company_url} target="_blank" className="min-h-[175px] rounded-3xl w-[300px] flex items-center justify-center">
+          <Link href={company_url} target="_blank" className="min-h-[175px] rounded-3xl w-[300px] max-h-[175px] flex items-center justify-center">
             <Image loader={myLoader} src={Logo.data.attributes.url} width={100} height={100} alt="" className="z-10 mt-0 w-full object-fill w-full"/>
           </Link>
         </div>
